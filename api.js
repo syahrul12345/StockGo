dce0a6e3b949a899f5c4ae793a20521a5042b333
Your Name <muhdsyahrulnizam123@gmail.com>
2019-10-05 14:21:13 +0800

    added singapore markets

--- api.js
+++ api.js
@@ -2,14 +2,25 @@ var axios = require('axios')
 
 
 const getPrice = async (ticker) => {
-    var baseUrl = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?region=SG&lang=en-SG`
+    var usAPI = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?region=SG&lang=en-SG`
+    var sgAPI = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}.si?region=SG&lang=en-SG`
+    let price = await getFromAPI(usAPI)
+    //check if usPrice is false, if it is recall the API using the sgAPI
+    if(price == false) {
+        price = getFromAPI(sgAPI)
+    }
+    return price
+}
+const getFromAPI = async (baseUrl) => {
     return axios.get(baseUrl).then((response) => {
+        console.log("resposne")
         // console.log(response.data.chart.result[0].meta)
         return [response.data.chart.result[0].meta.regularMarketPrice,response.data.chart.result[0].meta.chartPreviousClose]
-    }).catch((error) => {
+    }).catch(() => {
         return false
     })
 }
+
 module.exports = {
     getPrice
 }
